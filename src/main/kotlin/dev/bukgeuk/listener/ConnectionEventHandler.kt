59d2cbd88786bc49255a06f94169fe9b47907fa9
공지운 <contact@bukgeuk.dev>
2022-09-12 16:02:37 +0900

    health attribute

--- src/main/kotlin/dev/bukgeuk/listener/ConnectionEventHandler.kt
+++ src/main/kotlin/dev/bukgeuk/listener/ConnectionEventHandler.kt
@@ -6,23 +6,33 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
 class ConnectionEventHandler(private val healthScale: Double,
-                             private val AtkVariableName: String, private val AtkIncreaseGap: Double,
-                             private val AgiVariableName: String, private val AgiGap: Double
+                             private val hpVariableName: String, private val hpIncreaseGap: Double,
+                             private val atkVariableName: String, private val atkIncreaseGap: Double,
+                             private val agiVariableName: String, private val agiGap: Double
                              ): Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val vm = MagicSpells.getVariableManager()
+        val p = event.player
+
         val scale = vm.getVariable("meta_health_scale")
+
+        val hp = vm.getVariable("meta_attribute_generic_max_health_base")
+        val hpLevel = vm.getValue(hpVariableName, p)
+        val hpValue = hpLevel * (2 * hpIncreaseGap + (hpLevel - 1) * hpIncreaseGap) / 2
+
         val atk = vm.getVariable("meta_attribute_generic_attack_damage_base")
-        val atkLevel = vm.getValue(AtkVariableName, event.player)
-        val atkValue = atkLevel * (2 * AtkIncreaseGap + (atkLevel - 1) * AtkIncreaseGap) / 2
+        val atkLevel = vm.getValue(atkVariableName, p)
+        val atkValue = atkLevel * (2 * atkIncreaseGap + (atkLevel - 1) * atkIncreaseGap) / 2
+
         val agi = vm.getVariable("meta_attribute_generic_movement_speed_base")
-        val agiLevel = vm.getValue(AgiVariableName, event.player)
-        val agiValue = agiLevel * AgiGap
+        val agiLevel = vm.getValue(agiVariableName, p)
+        val agiValue = agiLevel * agiGap
 
-        if (scale.getValue(event.player) != healthScale) scale.set(event.player, healthScale)
+        if (scale.getValue(p) != healthScale) scale.set(p, healthScale)
 
-        atk.set(event.player, atk.getValue(event.player) + atkValue)
-        agi.set(event.player, agi.getValue(event.player) + agiValue)
+        hp.set(p, hp.getValue(p) + hpValue)
+        atk.set(p, atk.getValue(p) + atkValue)
+        agi.set(p, agi.getValue(p) + agiValue)
     }
 }
